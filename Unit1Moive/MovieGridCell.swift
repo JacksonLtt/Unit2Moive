7988b549e5a093004b27f30ec9dba53033a874e7
廷 <jacksonltt623@gmail.com>
2019-01-29 22:58:11 -0800

    Adding the liking feature

---
+++ Unit1Moive/MovieGridCell.swift
@@ -0,0 +1,16 @@
+//
+//  MovieGridCell.swift
+//  Unit1Moive
+//
+//  Created by Jackson Lu on 1/29/19.
+//  Copyright © 2019 Jackson Lu. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
